4520a6ad3fad0e2425b7c24e8a1266dc0c754f82
Rodney <admin@mobilewiseguy.net>
2021-02-17 15:53:42 -0600

    Initial Commit

---
+++ TTJ001/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TTJ001
+//
+//  Created by Rodney Hermes on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
